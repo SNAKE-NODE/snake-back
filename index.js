3394fff55c9f26203b859a6d1c431dc9d11d2487
adriandearias <deariasadrian@gmail.com>
2023-03-08 18:33:18 +0100

    added index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 const playersRoutes = require("./src/api/players/players.routes");
+const topRoutes = require("./src/api/players/top.routes");
 
 const db = require('./src/utils/db');
 db.connectDB();
@@ -13,9 +14,27 @@ const server = express();
 const cors = require('cors');
 server.use(cors());
 
+server.use('/tops', topRoutes);
+server.use('/players', playersRoutes);
+
 
 //! MIDDLEWARES
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
 
+
+server.listen(PORT, () =>{
+    console.log("Server is running! Ok -> http://localhost:"+PORT);
+});
+
+//! Errors control <- 4 params -> err, req, res, next
+server.use((err, req, res, next) => {
+    return res.status(err.status || 500).json(err.message || "Unknown Error");
+});
+
+server.use("*", (req, res, next) => {
+    const error = new Error("Route not found");
+    error.status = 404;
+    next(error); 
+});
