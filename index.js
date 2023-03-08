91e31046d3affa43043ba0457b5044cd232d700a
fpascuales <fpascuales@gmail.com>
2023-03-08 21:24:20 +0100

    la actualización de las 21:24

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT;
 const playersRoutes = require("./src/api/players/players.routes");
 const topRoutes = require("./src/api/top/top.routes");
 
-
-
 const express = require("express");
 const server = express();
 
@@ -23,15 +21,6 @@ server.use('/players', playersRoutes);
 const db = require('./src/utils/db');
 db.connectDB();
 
-server.use('/', (req, res)=>{
-    res.send('It Works!')
-})
-
-
-server.listen(PORT, () =>{
-    console.log("Server is running!");
-});
-
 //! Errors control <- 4 params -> err, req, res, next
 server.use((err, req, res, next) => {
     return res.status(err.status || 500).json(err.message || "Unknown Error");
@@ -41,4 +30,13 @@ server.use("*", (req, res, next) => {
     const error = new Error("Route not found");
     error.status = 404;
     next(error); 
+});
+
+server.use('/', (req, res)=>{
+    res.send('It Works!')
+})
+
+
+server.listen(PORT, () =>{
+    console.log("Server is running!");
 });
