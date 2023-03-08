ef49605b15beedfee57b4c35794d4ed2f6bc8001
fpascuales <fpascuales@gmail.com>
2023-03-08 21:13:44 +0100

    la actualización de las 21:13

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
+//dependencies
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const playersRoutes = require("./src/api/players/players.routes");
+const topRoutes = require("./src/api/top/top.routes");
+
+const db = require('./src/utils/db');
+db.connectDB();
+
 const express = require("express");
 const server = express();
 
-//dependenciess
-require('dotenv').config();
-
 const cors = require('cors');
-
-const PORT = process.env.PORT;
-
 server.use(cors());
 
 //! MIDDLEWARES
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
-const playersRoutes = require("./src/api/players/players.routes");
-const topRoutes = require("./src/api/top/top.routes");
-
 server.use('/tops', topRoutes);
 server.use('/players', playersRoutes);
 
-const db = require('./src/utils/db');
-db.connectDB();
+server.use('/', (req, res)=>{
+    res.send('It Works!')
+})
+
+
+server.listen(PORT, () =>{
+    console.log("Server is running!");
+});
 
 //! Errors control <- 4 params -> err, req, res, next
 server.use((err, req, res, next) => {
@@ -32,12 +39,4 @@ server.use("*", (req, res, next) => {
     const error = new Error("Route not found");
     error.status = 404;
     next(error); 
-});
-
-server.use('/', (req, res)=>{
-    res.send('It Works!')
-})
-
-server.listen(PORT, () =>{
-    console.log("Server is running!");
 });
