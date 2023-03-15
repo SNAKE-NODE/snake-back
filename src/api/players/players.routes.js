02748550ad057665f5408217f313edfe0f77169e
fpascuales <fpascuales@gmail.com>
2023-03-15 21:23:54 +0100

    modificado getAllPlayers por getPlayersById

--- src/api/players/players.routes.js
+++ src/api/players/players.routes.js
@@ -1,6 +1,6 @@
-const { newPLayer, login, getAllPlayers } = require("./players.controllers");
+const { newPLayer, login, getPlayersById } = require("./players.controllers");
 const playersRoutes = require("express").Router()
 playersRoutes.post("/registro", newPLayer)
 playersRoutes.post("/login", login)
-playersRoutes.get("/", getAllPlayers)
+playersRoutes.get("/:id", getPlayersById)
 module.exports = playersRoutes
